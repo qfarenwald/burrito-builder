111e39fa0c8b170586dec7b783039c2940b304a0
Quinne Farenwald <q.farenwald@gmail.com>
2019-11-06 09:21:51 -0700

    Console log props

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -14,6 +14,7 @@ export class App extends Component {
   }
 
   componentDidMount() {
+    console.log('props', this.props)
     getOrders()
       .then(data => this.props.setOrders(data.orders))
       .catch(err => console.error('Error fetching:', err));
